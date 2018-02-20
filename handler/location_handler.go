26daed405def88d1274dbf8ed962b01f3eb45475
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-02-20 19:08:05 +0900

    refactor err in location_handler

--- handler/location_handler.go
+++ handler/location_handler.go
@@ -7,54 +7,54 @@ import (
 	"strconv"
 )
 
-func GetLocation(c echo.Context) error {
+func GetLocation(c echo.Context) (err error) {
 	id, _ := strconv.Atoi(c.Param("id"))
 	loc, err := location.Get(id)
 	if err != nil {
-		return err
+		return
 	}
 	return c.JSON(http.StatusOK, &loc)
 }
 
-func GetAllLocations(c echo.Context) error {
+func GetAllLocations(c echo.Context) (err error) {
 	loc, _ := location.All()
 	return c.JSON(http.StatusOK, &loc)
 }
 
-func PostLocation(c echo.Context) error {
+func PostLocation(c echo.Context) (err error) {
 	loc := new(location.Location)
-	err := c.Bind(loc)
+	err = c.Bind(loc)
 	if err != nil {
-		return err
+		return
 	}
 	err = loc.Create()
 	if err != nil {
-		return err
+		return
 	}
 	return c.JSON(http.StatusOK, &loc)
 }
 
-func PutLocation(c echo.Context) error {
+func PutLocation(c echo.Context) (err error) {
 	id, _ := strconv.Atoi(c.Param("id"))
 	loc, _ := location.Get(id)
 	newLoc := new(location.Location)
-	err := c.Bind(newLoc)
+	err = c.Bind(newLoc)
 	if err != nil {
-		return err
+		return
 	}
 	err = loc.Update(*newLoc)
 	if err != nil {
-		return err
+		return
 	}
 	return c.String(http.StatusOK, "put!")
 }
 
-func DeleteLocation(c echo.Context) error {
+func DeleteLocation(c echo.Context) (err error) {
 	id, _ := strconv.Atoi(c.Param("id"))
 	loc, _ := location.Get(id)
-	err := loc.Delete()
+	err = loc.Delete()
 	if err != nil {
-		return err
+		return
 	}
 	return c.String(http.StatusOK, "deleted!")
 }
